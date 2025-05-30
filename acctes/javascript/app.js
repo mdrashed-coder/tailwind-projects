7cef65d4f0035a53484af9f99763870de61c41cc
mdrashed-coder <mdrashed976142@gmail.com>
2025-05-30 10:41:17 +0600

    Tailwind css project

---
+++ acctes/javascript/app.js
@@ -0,0 +1,11 @@
+// let mobile_icon = document.getElementById("mobile_icon");
+// let mobile_menu = document.getElementById("mobile_menu");
+
+// mobile_icon.addEventListener("click", ()=>{
+//     mobile_menu.classList.toggle("hidden");
+// })
+let manu_icon = document.getElementById("manu_icon");
+let manu_list = document.getElementById("manu_list");
+manu_icon.addEventListener("click",() => {
+    manu_list.classList.toggle("hidden");
+})
